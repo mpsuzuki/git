8b1c9623dc70b2babbeb5fbd5fd4ee4201ede50b
suzuki toshiya <mpsuzuki@hiroshima-u.ac.jp>
2017-12-27 13:08:21 +0900

    git-archive: accepts --owner --group aslike GNU tar.
    Current tar output by git-archive has always root:root.
    To generate tar output with non-root owner/group,
    the options like GNU tar are added.
    
    * archive.h: add members 'uid', 'gid', 'uname', 'gname'
      to struct archiver_args.
    
    * archive.c: add functions to reflect the operands of
      '--owner' and '--group' to archiver_args.
    
    * archive-tar.c: copy 'uid', 'gid', 'uname', 'gname'
      from archiver_args to the entry headers in tar archive.
    
    * t/parse-tar-file.py: a script to dump uid, gid, uname,
      gname fields from a tar archive.
    
    * t/t5005-archive-uid-gid.sh: a test script comparing
      uid, gid, uname, gname between the options and
      generated tar file.

--- archive.h
+++ archive.h
@@ -15,6 +15,10 @@ struct archiver_args {
 	unsigned int worktree_attributes : 1;
 	unsigned int convert : 1;
 	int compression_level;
+	int uid;
+	int gid;
+	const char *uname;
+	const char *gname;
 };
 
 #define ARCHIVER_WANT_COMPRESSION_LEVELS 1
