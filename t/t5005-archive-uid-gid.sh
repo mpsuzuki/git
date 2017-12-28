843bf790cdf842cea4644c2c0972d2412d8db89c
suzuki toshiya <mpsuzuki@hiroshima-u.ac.jp>
2017-12-28 15:05:58 +0900

    make a test script for --owner --group options runnable

--- t/t5005-archive-uid-gid.sh
+++ t/t5005-archive-uid-gid.sh
@@ -31,7 +31,7 @@ check_uid_gid_uname_gname_in_tar() {
 	  return $?
         fi
 
-	gname=`./parse-tar-file.py --print=uname --fail-if-multi $1`
+	gname=`./parse-tar-file.py --print=gname --fail-if-multi $1`
 	if test $? != 0 -o x"${gname}" != "x"$5
 	then
 	  echo "(some) gname differs from the specified value"
@@ -41,24 +41,31 @@ check_uid_gid_uname_gname_in_tar() {
 	return 0
 }
 
+git init . 1>/dev/null 2>/dev/null
+touch uid-gid-test.001
+mkdir uid-gid-test.002
+mkdir uid-gid-test.002/uid-gid-test.003
+git add uid-gid-test.001
+git add uid-gid-test.002
+git add uid-gid-test.002/uid-gid-test.003
+git commit -m "uid-gid-test" 2>/dev/null 1>/dev/null
 
-test_expect_success 'test explicitly specified cases, owner=nobody:1234 group=nobody:5678' '
-	git archive --format=tar --owner nobody:1234 --gid nogroup:5678 HEAD > uid-gid-test.tar
-	check_uid_gid_uname_gname_in_tar(uid-gid-test.tar, "1234", "5678", "nobody", "nogroup")
-	test_result=$?
-	rm -f uid-gid-test.tar
+test_expect_success 'test a case with explicitly specified name/id, owner=nobody:1234 group=nogroup:5678' '
+	git archive --format=tar --owner nobody:1234 --group nogroup:5678 HEAD > uid-gid-test1.tar &&
+	check_uid_gid_uname_gname_in_tar uid-gid-test1.tar 1234 5678 nobody nogroup &&
+	test_result=$? &&
 	return ${test_result}
 '
 
-test_expect_success 'test only name specified cases, owner=(current my name) group=(current my group)' '
-	my_uid=`id -u`
-	my_gid=`id -g`
-	my_uname=`id -u -n`
-	my_gname=`id -g -n`
-	git archive --format=tar --owner ${my_uname} --gid ${my_gname} HEAD > uid-gid-test.tar
-	check_uid_gid_uname_gname_in_tar(uid-gid-test.tar, my_uid, my_gid, my_uname, my_gname)
-	test_result=$?
-	rm -f uid-gid-test.tar
+test_expect_success 'test a case with only name is specified, owner=(current my name) group=(current my group)' '
+	my_uid=`id -u` &&
+	my_gid=`id -g` &&
+	my_uname=`id -u -n` &&
+	my_gname=`id -g -n` &&
+	git archive --format=tar --owner ${my_uname} --group ${my_gname} HEAD > uid-gid-test2.tar &&
+	check_uid_gid_uname_gname_in_tar uid-gid-test2.tar ${my_uid} ${my_gid} ${my_uname} ${my_gname} &&
+	test_result=$? &&
 	return ${test_result}
 '
+
 test_done
