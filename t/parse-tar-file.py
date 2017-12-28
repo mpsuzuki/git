d2243dc26938b5a0752de82a37130c39521dc52a
suzuki toshiya <mpsuzuki@hiroshima-u.ac.jp>
2017-12-28 13:15:21 +0900

    test files for git-archive --owner --group options

---
+++ t/parse-tar-file.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+
+import sys
+import getopt
+import tarfile
+
+optlist, args = getopt.getopt( sys.argv[1:], "", [
+				"print=", "show=",
+				"uniq",
+				"fail-if-multi",
+		] )
+
+infos_to_print = []
+uniq = False
+fail_if_multi = False
+
+for opt in optlist:
+  if opt[0] == "--print":
+    infos_to_print.append(opt[1])
+  elif opt[0] == "--show":
+    infos_to_print.append(opt[1])
+  elif opt[0] == "--uniq":
+    uniq = True
+  elif opt[0] == "--fail-if-multi":
+    uniq = True
+    fail_if_multi = True
+
+if len(infos_to_print) == 0:
+  infos_to_print = ["uid", "gid", "uname", "gname", "name"]
+
+tar = tarfile.open( args[0] )
+out_lines = []
+for tarinfo in tar:
+  infos = []
+  for info_tag in infos_to_print:
+    if info_tag == "uid":
+      infos.append( str(tarinfo.uid) )
+    elif info_tag == "gid":
+      infos.append( str(tarinfo.gid) )
+    elif info_tag == "uname" or info_tag == "owner":
+      infos.append( tarinfo.uname )
+    elif info_tag == "gname" or info_tag == "group":
+      infos.append( tarinfo.gname )
+    elif info_tag == "name" or info_tag == "pathname":
+      infos.append( tarinfo.name )
+  out_lines.append( "\t".join(infos) )
+tar.close()
+
+if uniq:
+  out_lines = list(set(out_lines))
+  if fail_if_multi and (len(out_lines) > 1):
+    sys.stderr.write("*** not unique value, " + str(len(out_lines)) + " values found\n")
+    sys.exit(len(out_lines))
+
+for line in out_lines:
+  print line
