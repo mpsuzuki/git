9b772dfa21c9c03b94f46af91eace15fe538c632
suzuki toshiya <mpsuzuki@hiroshima-u.ac.jp>
2017-12-29 22:07:58 +0900

    move repository initialization for tests into first test

--- t/t5005-archive-uid-gid.sh
+++ t/t5005-archive-uid-gid.sh
@@ -3,6 +3,17 @@
 test_description='test --owner --group options for git-archive'
 . ./test-lib.sh
 
+test_expect_success 'create commit with a few empty files' '
+	git init . 1>/dev/null 2>/dev/null &&
+	touch uid-gid-test.001 &&
+	mkdir uid-gid-test.002 &&
+	mkdir uid-gid-test.002/uid-gid-test.003 &&
+	git add uid-gid-test.001 &&
+	git add uid-gid-test.002 &&
+	git add uid-gid-test.002/uid-gid-test.003 &&
+	git commit -m "uid-gid-test" 2>/dev/null 1>/dev/null
+'
+
 check_uid_gid_uname_gname_in_tar() {
 	# $1 tar pathname
 	# $2 uid (digit in string)
@@ -12,43 +23,34 @@ check_uid_gid_uname_gname_in_tar() {
 	uid=`python "$TEST_DIRECTORY"/t5005/parse-tar-file.py --print=uid --fail-if-multi $1`
 	if test $? != 0 -o x"${uid}" != "x"$2
 	then
-	  echo "(some) uid differs from the specified value"
-	  return $?
-        fi
+		echo "(some) uid differs from the specified value"
+		return $?
+	fi
 
 	gid=`python "$TEST_DIRECTORY"/t5005/parse-tar-file.py --print=gid --fail-if-multi $1`
 	if test $? != 0 -o x"${gid}" != "x"$3
 	then
-	  echo "(some) gid differs from the specified value"
-	  return $?
-	  exit $?
-        fi
+		echo "(some) gid differs from the specified value"
+		return $?
+		exit $?
+	fi
 
 	uname=`python "$TEST_DIRECTORY"/t5005/parse-tar-file.py --print=uname --fail-if-multi $1`
 	if test $? != 0 -o x"${uname}" != "x"$4
 	then
-	  echo "(some) uname differs from the specified value"
-	  return $?
-        fi
+		echo "(some) uname differs from the specified value"
+		return $?
+	fi
 
 	gname=`python "$TEST_DIRECTORY"/t5005/parse-tar-file.py --print=gname --fail-if-multi $1`
 	if test $? != 0 -o x"${gname}" != "x"$5
 	then
-	  echo "(some) gname differs from the specified value"
-	  return $?
-        fi
+		echo "(some) gname differs from the specified value"
+		return $?
+	fi
 
 	return 0
 }
-
-git init . 1>/dev/null 2>/dev/null
-touch uid-gid-test.001
-mkdir uid-gid-test.002
-mkdir uid-gid-test.002/uid-gid-test.003
-git add uid-gid-test.001
-git add uid-gid-test.002
-git add uid-gid-test.002/uid-gid-test.003
-git commit -m "uid-gid-test" 2>/dev/null 1>/dev/null
 
 test_expect_success 'test a case with explicitly specified name/id, owner=nobody:1234 group=nogroup:5678' '
 	git archive --format=tar --owner nobody:1234 --group nogroup:5678 HEAD > uid-gid-test1.tar &&
